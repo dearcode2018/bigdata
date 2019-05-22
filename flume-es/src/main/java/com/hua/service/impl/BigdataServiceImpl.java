a5cfe6ee1f0e63e215be760d1307c9c493597557
qianye.zheng <qianye.zheng@wehotelglobal.com>
2019-05-22 21:08:47 +0800

    no message

---
+++ flume-es/src/main/java/com/hua/service/impl/BigdataServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * BigdataServiceImpl.java
+ * @author  qye.zheng
+ * 	version 1.0
+ */
+package com.hua.service.impl;
+
+import com.hua.service.BigdataService;
+
+/**
+ * BigdataServiceImpl
+ * 描述: BigData - 服务
+ * @author  qye.zheng
+ */
+public final class BigdataServiceImpl extends CoreServiceImpl implements
+		BigdataService
+{
+
+}
